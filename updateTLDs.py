7ea82de73fc11717717bf59de4b8b8bacc5c4cd1
Walter Leibbrandt <git wrl co za>
2016-02-02 20:16:35 +0200

    PEP 8 and PEP 257

--- updateTLDs.py
+++ updateTLDs.py
@@ -1,9 +1,13 @@
-import time, urllib2, os.path
 """
-This should update the TLDs list every few days if you plug this into your main application. There may be some variance in time.time()
-and system local timezones (unsure, didn't test). Also maybe some issues with st_mtime depending on your operating system.
+This should update the TLDs list every few days if you plug this into
+your main application. There may be some variance in time.time() and
+system local timezones (unsure, didn't test). Also maybe some issues
+with st_mtime depending on your operating system.
 """
 
+import time
+import urllib2
+import os.path
 
 # one day = 86,400 seconds
 day = 86400
@@ -15,12 +19,14 @@ start_time = time.time()
 
 def update_tlds(location, source):
     """
-    Downloads a list of TLDs from 'source' and stores the TLDs in list 'new_tlds'.
-    Compares 'new_tlds' to a local file at 'location', and creates a file there if it does not exist.
-    (If you get write errors, it may be due to your file permissions.)
-    If there are 'new_tlds' not in 'old_tlds' it will rewrite the file with the current 'new_tlds'
-    This could be further expanded with runtime args for print_status, and whether to overwrite the file or just \
-    append the diff between the sets.
+    Downloads a list of TLDs from 'source' and stores the TLDs in list
+    'new_tlds'.
+
+    Compares 'new_tlds' to a local file at 'location', and creates a
+    file there if it does not exist.  (If you get write errors, it may
+    be due to your file permissions.) If there are 'new_tlds' not in
+    'old_tlds' it will rewrite the file with the current 'new_tlds'.
+
     :param location: the location of the local file
     :param source: the location of the remote file
     """
@@ -44,7 +50,8 @@ def update_tlds(location, source):
                 for tld in new_tlds:
                     ff.write(tld + '\n')
             status += 'Success, list has been updated.\n' + \
-                      'TLDs Added: ' + str(list(set(new_tlds)-set(old_tlds))) + '\n'
+                      'TLDs Added: ' + \
+                      str(list(set(new_tlds)-set(old_tlds))) + '\n'
 
     except Exception as e:
         status = e
@@ -62,4 +69,5 @@ if time_elapsed > run_every:
     updated = update_tlds(loc, src)
     print updated
 else:
-    print "TLDs last updated about " + str(round((time_elapsed/day), 2)) + " days ago."
+    print "TLDs last updated about " + str(round((time_elapsed/day), 2)) + \
+          " days ago."
